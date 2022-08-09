7612ccac8ae0992c6fb082208599f35f5a520523
baochi0212 <chideptraika29@gmail.com>
2022-08-09 23:01:58 +0700

    data.sh

--- NLP/implement/temp/data/download_data.sh
+++ NLP/implement/temp/data/download_data.sh
@@ -3,4 +3,4 @@ wget http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
 mkdir raw
 mv 'cornell_movie_dialogs_corpus.zip' 'raw'
 unzip 'raw/cornell_movie_dialogs_corpus.zip'
-!mv "/content/deep-learning/NLP/implement/temp/data/cornell movie-dialogs corpus" raw
+mv "/content/deep-learning/NLP/implement/temp/data/cornell movie-dialogs corpus" raw
