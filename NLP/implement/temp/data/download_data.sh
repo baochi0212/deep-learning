b175b005111204e2fd8f03068b85cf83f8b04b7c
baochi0212 <chideptraika29@gmail.com>
2022-08-20 22:10:01 +0700

    rm

--- NLP/implement/temp/data/download_data.sh
+++
@@ -1,6 +0,0 @@
-# move towards data folder first
-wget http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
-mkdir raw
-mv 'cornell_movie_dialogs_corpus.zip' 'raw'
-unzip 'raw/cornell_movie_dialogs_corpus.zip'
-mv "/content/deep-learning/NLP/implement/temp/data/cornell movie-dialogs corpus" raw
